c7908e1d06c12a92aab587bb3ad0229c61c75e49
Aaron Cox <aaron.cox@greymass.com>
2013-04-30 10:54:29 -0500

    Fixed formatting.

--- nodejs/build.js
+++ nodejs/build.js
@@ -1,25 +1,25 @@
 var mongoose = require('mongoose'),
-		Schema = mongoose.Schema,
-		ObjectId = Schema.ObjectId;
+	Schema = mongoose.Schema,
+	ObjectId = Schema.ObjectId;
  
 var buildSchema = new Schema({name: String}, { collection : 'builds' });
 
 buildSchema.methods.toJSON = function() {
 	var fields = {
-		'id'						: null,
-		'name'					: null,
-		'class'					: 'heroClass',
-		'level'					: null,
-		'hardcore'			: null,
-		'paragon'				: null,
-		'actives'				: null,
-		'passives'			: null,
-		'_characterId'	: 'bt-id',
-		'_characterBt'	: 'bt-tag',
-		'_characterRg'	: 'bt-srv'
+		'id': null,
+		'name': null,
+		'class': 'heroClass',
+		'level': null,
+		'hardcore': null,
+		'paragon': null,
+		'actives': null,
+		'passives': null,
+		'_characterId': 'bt-id',
+		'_characterBt': 'bt-tag',
+		'_characterRg': 'bt-srv'
 	};
 	var data = {};
-  obj = this.toObject()
+			obj = this.toObject()
 	if(obj) {
 		Object.keys(fields).forEach(function(k,v) {
 			if(obj[k]) {
@@ -28,13 +28,13 @@ buildSchema.methods.toJSON = function() {
 		});
 		if(obj['stats']) {
 			if(obj['stats']['dps']) {
-				data['dps'] = obj['stats']['dps'];				
+				data['dps'] = obj['stats']['dps'];	
 			}
 			if(obj['stats']['dps']) {
 				data['ehp'] = obj['stats']['ehp'];
 			}
 		}
-	  return data;		
+	  return data;	
 	}
 	return obj;
 }
